f269b72e7f27cfad5616e855957dd4d02ca223f6
steingartpeter <steingartpeter@users.noreply.github.com>
2022-11-20 08:14:59 +0100

    ChildToParent 2022-11-20 001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wsearch';
+  getTerm(term: string) {
+    console.log('Im the app, and the term arrived is: ' + term);
+  }
 }
